9ec7b40eeb2288456a9fc1edcfc7105f8d484d0a
Concillia Mpofu <97482839+ConcilliaHlezi@users.noreply.github.com>
2022-11-01 23:17:20 -0400

    Powerpack Package

---
+++ R/my_sq.R
@@ -0,0 +1,4 @@
+#' @keywords internal
+my_sq <- function (x){
+  x^2
+}
---
+++ R/powerpack.R
@@ -0,0 +1,11 @@
+#' powerpack: Adding squares abd cubes
+#'
+#' The powerpack package provides function that calculates
+#' either squares or cubes of two numbers, then add
+#'
+#' @section Arbitary Sections:
+#' You can add arbitraty sections to your documentation,
+#'
+#' @docType package
+#' @name powerpack-package
+"_PACKAGE"
